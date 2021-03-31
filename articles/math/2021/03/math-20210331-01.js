ee0245c04f5cffcf1e65dd5fe0374b0239347113
kazuhisa honda <ringhomomorphism0to0@yahoo.co.jp>
2021-03-31 21:23:01 +0900

    記事、index.html追加

---
+++ articles/math/2021/03/math-20210331-01.js
@@ -0,0 +1,33 @@
+MultipePlatformBlogData.register(doc => {
+    const { tex, attr } = doc
+    const { d, A, B, M, E } = tex.canonicalSymbols
+    const { p, div, table, br, h4 } = doc.el
+    const justifyCenter = attr("style", "display:flex; justify-content: center;")
+    const b1_b2 = tex("b_1, b_2")
+    const A_alg = tex("A[b_1, b_2]")
+    const href = link => attr("href", link)
+
+
+    doc.articleId("math-20210331-01")
+    doc.title("整な元は加法、乗法で閉じている")
+    doc.body(
+        h4("記号の定義"),
+        div(justifyCenter,
+            table(
+                [A, "可換環の拡大"],
+                [B, [A, "を含む可換環"]],
+                [b1_b2, [B, "の元"]],
+                [A_alg, [b1_b2, "で生成される", B, "の部分環"]],
+            ),
+        ),
+        h4("定理"),
+        p(tex("b_1, b_2∈B"), "が", A, "上整であれば、", tex("b_1 + b_2, 　b_1b_2"), "も", A, "上整である"),
+        h4("証明"),
+        p(b1_b2, "が", A, "上整だから、", A_alg, "は", A, "加群として有限生成であり、",
+            tex("b_1+b_2, b_1b_2∈", A_alg), "だから、",
+            doc.el.a(href(doc.articleLink("math-20210328-01")), "こちら"), "の系から",
+            tex("b_1+b_2, b_1b_2"), "も", A, "上整であることがわかる。"
+        ),
+        p("（証明終）")
+    )
+})
