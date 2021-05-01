601a0070ecce9dda47277897d32a217b47851d26
kazuhisa honda <ringhomomorphism0to0@yahoo.co.jp>
2021-05-01 22:07:58 +0900

    記事リンク追加

--- articles/math/2021/04/math-20210418-02.js
+++ articles/math/2021/04/math-20210418-02.js
@@ -14,24 +14,19 @@ MultipePlatformBlogData.register(doc => {
             table(
                 [[tex("L/K"), textRight], "可換体の有限次代数拡大"],
                 [[tex("L_s"), textRight], [L, "における", K, "の分離閉包"]],
-                [[tex("a"), textRight], [
-                    tex("L_s = K(a)"), "となる元", tex("∈L_s"),
-                    br(), "（TODO:有限次分離拡大が単純拡大であることの証明記事リンク）"
-                ]],
             ),
         )
         , h4("定理")
         , p(tex("L/K"), "が単純拡大", tex("⇔ L/L_s"), "が単純拡大")
         , h4("証明")
         , p(tex("⇒"), "は明らか。", tex("⇐"), "を示す。")
-        , p(tex("L_s/K"), "が単純拡大なので（TODO: 証明記事リンク）", tex("L_s = K(a)　(a∈L_s)"), "と置く。")
+        , p(tex("L_s/K"), "が単純拡大なので（", doc.article("math-20210418-03", "こちら"), "を参照）", tex("L_s = K(a)　(a∈L_s)"), "と置く。")
         , p(tex("L = L_s = K(a)"), "の場合は良いので、", tex("L ≠ L_s"), "とする。")
-        , p(K, "は無限体である。", br(), "（TODO: 有限体が完全体であることの証明記事リンク）")
+        , p(K, "は無限体である（"
+            , doc.article("math-20210430-01", "有限体は完全体"), "なので"
+            , tex("L = L_s"), "となってしまう）。")
         , p(tex("L = L_s(b)"), "と置くと、", tex("L = K(a, b)"), "で、"
-            , doc.el.a(
-                doc.attr("href", doc.articleLink("math-20210418-01"))
-                , "こちらの定理"
-            )
+            , doc.article("math-20210418-01", "こちらの定理")
             , "より、ある", tex("c∈K"), "が存在して"
             , tex("K(a, b) = K(cb + a)")
         )
